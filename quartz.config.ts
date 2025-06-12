8a8cd0a0e29ca76798b025cca166981d6f13f0e3
Krishnanand V P <vpkrishnanand@gmail.com>
2025-06-12 12:34:06 +0530

    chnange site address

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "krishna.dev/blog",
+    baseUrl: "krishna.dev/notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
